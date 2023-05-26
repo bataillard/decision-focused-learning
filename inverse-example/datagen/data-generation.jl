0fca270f02b423719fe8b8b3e18342db35b5da08
Luca Bataillard <lucabad@bluewin.ch>
2023-05-26 10:45:07 -0400

    add noisy datagen

--- inverse-example/datagen/data-generation.jl
+++ inverse-example/datagen/data-generation.jl
@@ -1,6 +1,6 @@
 module DataGeneration
 
-using Gurobi, JuMP, LinearAlgebra, Distributions
+using Gurobi, JuMP, LinearAlgebra, Distributions, PDMats
 
 import ..Forward as Forward
 import ..InverseLinReg as IOLinReg
@@ -9,42 +9,44 @@ export DataGenParams
 export generate_solution_points
 
 struct DataGenParams
-    n_commodities::Integer
     n_features::Integer
     weights::Matrix
     noise_variance::Union{Vector, Nothing}
 
     function DataGenParams(; weights::Matrix, noise_variance=nothing::Union{Vector, Nothing})
-        n_commodities, n_features = size(weights)
+        n_features = size(weights)[2]
         
-        if (noise_variance !== nothing && size(noise_variance) !== n_commodities)
-            error("Invalid size $(size(noise_variance)) of noise variance, should be $(n_commodities)")
+        if (noise_variance !== nothing && size(noise_variance)[1] != n_features)
+            error("Invalid size $(size(noise_variance)) of noise variance, should be $(n_features)")
         end
 
-        new(n_commodities, n_features, weights, noise_variance)
+        new(n_features, weights, noise_variance)
     end
 end
 
 function generate_noise(datagen_params::DataGenParams)
     if datagen_params.noise_variance === nothing
-        return zeros(datagen_params.n_commodities)
+        return zeros(datagen_params.n_features)
     end
 
     covariance_matrix = PDiagMat(datagen_params.noise_variance)
-    distribution = DiagNormal(zeros(datagen_params.n_commodities), covariance_matrix)
+    distribution = DiagNormal(zeros(datagen_params.n_features), covariance_matrix)
 
     return rand(distribution, 1)
 end
 
-function predict_demand(datagen_params::DataGenParams, features::Vector)        
-    return datagen_params.weights * features + generate_noise(datagen_params)
+function predict_demand(datagen_params::DataGenParams, features)        
+    return datagen_params.weights * features
 end
 
 function generate_solution_points(datagen_params::DataGenParams, problem_params::IOLinReg.Params, featuress)
+    print(featuress)
+    
     demandss = [predict_demand(datagen_params, features) for features in featuress]
     forward_sols = [Forward.create_and_solve_problem(problem_params.forward_params, demands) for demands in demandss]
+    noisy_featuress = [features .+ generate_noise(datagen_params) for features in featuress]
 
-    return [IOLinReg.SolutionPoint(forward_sol, features) for (forward_sol, features) in zip(forward_sols, featuress)]
+    return [IOLinReg.SolutionPoint(forward_sol, vec(noisy_features)) for (forward_sol, noisy_features) in zip(forward_sols, noisy_featuress)]
 end
 
 end
